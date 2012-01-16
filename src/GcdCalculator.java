e4f1ad3ecdc63ab92891339da40479fc015e2db9
Shreyas Chand <shreyas.chand@gmail.com>
2012-01-16 16:55:32 -0500

    added work display to GCD calculator

--- src/GcdCalculator.java
+++ src/GcdCalculator.java
@@ -1,4 +1,6 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -11,6 +13,8 @@ import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 
@@ -18,6 +22,7 @@ public class GcdCalculator extends JFrame implements KeyListener, ActionListener
 	
 	private JTextField inputField;
 	private JLabel gcdLabel, lcmLabel, errorLabel;
+	private JTextArea workArea;
 	
 	public static void main (String [] args) {
 		new GcdCalculator();
@@ -33,11 +38,13 @@ public class GcdCalculator extends JFrame implements KeyListener, ActionListener
 		inputPanel.add(inputField);
 		
 		errorLabel = new JLabel();
+		JPanel errorPanel = new JPanel();
+		errorPanel.add(errorLabel);
 		
 		JPanel topPanel = new JPanel();
 		topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.Y_AXIS));
 		topPanel.add(inputPanel);
-		topPanel.add(errorLabel);
+		topPanel.add(errorPanel);
 		
 		getContentPane().add(BorderLayout.NORTH, topPanel);
 		
@@ -59,16 +66,34 @@ public class GcdCalculator extends JFrame implements KeyListener, ActionListener
 		midPanel.add(gcdPanel);
 		midPanel.add(lcmPanel);
 		
-		getContentPane().add(BorderLayout.SOUTH, midPanel);
+		JPanel bottomPanel = new JPanel();
+		workArea = new JTextArea();
+		workArea.setEditable(false);
 		
-		pack();
+		JScrollPane pane = new JScrollPane(workArea);
+		pane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		pane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		pane.setSize(450, 100);
+		pane.setPreferredSize(new Dimension(450,100));
+		bottomPanel.add(pane);
+		
+		JPanel southPanel = new JPanel();
+		southPanel.setLayout(new BoxLayout(southPanel, BoxLayout.Y_AXIS));
+		southPanel.add(midPanel);
+		southPanel.add(bottomPanel);
+		
+		getContentPane().add(BorderLayout.SOUTH, southPanel);
+		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setPreferredSize(new Dimension(500, 230));
+		setSize(500, 230);
 		setResizable(false);
 		setTitle("GCD and LCM");
 		setVisible(true);
 	}
 	
 	public void actionPerformed(ActionEvent ev) {
+		workArea.setText("");
 		String input = inputField.getText();
 		String numStrings[] = input.split(",");
 		
@@ -76,18 +101,24 @@ public class GcdCalculator extends JFrame implements KeyListener, ActionListener
 		
 		if(input.length() == 0) {
 			error = true;
-			errorLabel.setText("please enter something!");
+			errorLabel.setText("Please enter something!");
 		}else if (input.matches(".*[^0-9,]+.*")){
 			error = true;
-			errorLabel.setText("only numbers and commas are allowed!");
+			errorLabel.setText("Only positive numbers and commas are allowed!");
 		}else if (numStrings.length == 1) {
 			error = true;
-			errorLabel.setText("enter more than one number!");
+			errorLabel.setText("Enter more than one number!");
+		}else {
+			for(int i = 0; i != numStrings.length; i++) {
+				if(Integer.parseInt(numStrings[i]) <= 0){
+					error = true;
+					errorLabel.setText("Only positive numbers are allowed!");
+				}
+			}
 		}
-			
 		
 		if(error){
-			pack();
+			errorLabel.setForeground(Color.RED);
 			return;
 		}
 		
@@ -107,23 +138,37 @@ public class GcdCalculator extends JFrame implements KeyListener, ActionListener
 		
 		gcdLabel.setText(String.valueOf(gcd));
 		
-		lcmLabel.setText(String.valueOf(lcm(nums)));
+		long product = 1;
+		for(int i = 1; i < nums.size(); i++) {
+			product *= nums.get(i);
+		}
 		
-		pack();
+		lcmLabel.setText(String.valueOf(product/gcd));
+		
+		
 	}
 			
-	private static int gcd(int m, int n) {
-		
-		while (m>0) {	
+	private int gcd(int n, int m) {
+		int oldN, oldM;
+		while (m>0) {
 			int r = n % m;
+			
+			oldN = n;
+			oldM = m;
+			
 			n = m;
 			m = r;
+			if(m != 0) {
+				workArea.append("gcd(" + oldN + ", " + oldM + ") = gcd(" + n + ", " + m + ") since " + oldN + " = " + oldM + "(" + Integer.valueOf(oldN/oldM) + ") + " + r + "\n");
+			}else {
+				workArea.append("gcd(" + oldN + ", " + oldM + ") = gcd(" + n + ", " + m + ") = " + n + "\n");
+			}
 		}
 		return n;
 		
 	}
 	
-	private static long lcm(ArrayList<Integer> nums) {
+	private long lcm(ArrayList<Integer> nums) {
 		Integer max = Collections.max(nums);
 		nums.remove(max);
 		
@@ -145,8 +190,10 @@ public class GcdCalculator extends JFrame implements KeyListener, ActionListener
 	public void keyPressed(KeyEvent e) {}
 
 	public void keyReleased(KeyEvent e) {
-		if(e.getKeyCode() != KeyEvent.VK_ENTER)
+		if(e.getKeyCode() != KeyEvent.VK_ENTER){
 			errorLabel.setText("");
+			workArea.setText("");
+		}
 	}
 
 	public void keyTyped(KeyEvent e) {}
